e1f54b1ae55d8be4d4aa62e5cd99a3dcef13f0f8
unknown <airvw14@gmail.com>
2023-10-04 00:53:18 +0900

    fc

---
+++ src/main/kotlin/totheking/traveljournals/controller/PostController.kt
@@ -0,0 +1,10 @@
+package totheking.traveljournals.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PostController {
+    private PostService
+
+
+}
